2feb1d65485f932b10b087442a1d586fcadd9586
Ruan Marcos Gervasi <ruan@ruangervasi.com.br>
2020-08-31 23:38:31 -0300

    StartProject

---
+++ src/components/Header/index.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import logo from '../../assets/logo.svg';
+import './index.css';
+
+export default function Header() {
+ return (
+   <header className="container">
+       <Link to="/">
+        <img className="logo" src={logo} alt="Logo trips"/>
+       </Link>
+       <Link className="reserva" to="/reservas">
+           <div>
+               <strong>Minhas Reservas</strong>
+               <span>0 Reservas</span>
+           </div>
+       </Link>
+   </header>
+ );
+}
