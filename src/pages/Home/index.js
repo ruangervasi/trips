cdccf99cf4a869622f3f57258cff03dcc7aed245
Ruan Marcos Gervasi <ruan@ruangervasi.com.br>
2020-09-01 21:39:51 -0300

    Init

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,9 +1,41 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import api from '../../services/api';
+import {MdFlightTakeoff} from 'react-icons/md'
+
+import './index.css';
 
 export default function Home() {
+  const [trips, setTrips] = useState([]);
+
+  useEffect(() => {
+      async function loadApi(){
+        const response = await api.get('trips');
+        setTrips(response.data);
+
+        console.log(response.data);
+      }
+      loadApi();
+  },[]);
  return (
-   <div>
-       <h1>Home</h1>
-   </div>
+    <div>
+        <div className="box">
+            {
+                trips.map(trip => (
+                    <li key={trip.id}>
+                        <img src={trip.image} alt={trip.title}/>
+                        <strong>{trip.title}</strong>
+                        <span>Status: {trip.status ? 'Disponível' : 'Indisponivel'}</span>
+
+                        <button type="button" onClick={() => {}}>
+                            <div>
+                                <MdFlightTakeoff size={16} color="#FFF"/>
+                            </div>
+                            <span>Solicitar Reserva</span>
+                        </button>
+                    </li>
+                ))
+            }
+        </div>
+    </div>
  );
 }
--- src/pages/Reservas/index.js
+++ src/pages/Reservas/index.js
@@ -1,9 +1,27 @@
 import React from 'react';
+import api from '../../services/api';
+import {MdDelete} from 'react-icons/md'
+
+import './index.css';
 
 export default function Reservas() {
  return (
    <div>
-       <h1>Reservas</h1>
+       <h1 className="title">você solicitou  1 reservas</h1>
+
+       <div className="reservas">
+        <img src="https://sujeitoprogramador.com/wp-content/uploads/2019/12/maceio.jpg" alt="Teste"/>
+        <strong>Viagem Maceio 7 dias</strong>
+        <span>Quantidade: 2</span>
+        <button type="button" onClick={() => {}}>
+          <MdDelete sizes={30} color="#191919"/>
+        </button>
+       </div>
+       <footer>
+         <button type="button" onClick={() => {}}>Solicitar Reservas</button>
+       </footer>
    </div>
+
+
  );
 }
